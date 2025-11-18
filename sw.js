aafcfcc5bf621e7166445f94d694b1e36e6582bc
awadhalkelali01 <awadh.alkelali.01@gmail.com>
2025-11-18 22:31:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mywallet-cache-v1";
+const CACHE_NAME = "mywallet-cache-v2";
 
 // الملفات التي سيتم تخزينها Offline
 const ASSETS = [
@@ -59,3 +59,4 @@ self.addEventListener("activate", event => {
     )
   );
 });
+
