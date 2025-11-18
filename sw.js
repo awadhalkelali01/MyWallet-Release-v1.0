d2aa41aae3663747603184b422f3d05d615a9756
awadhalkelali01 <awadh.alkelali.01@gmail.com>
2025-11-18 14:35:22 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = "mywallet-cache-v1";
+
+// الملفات التي سيتم تخزينها Offline
+const ASSETS = [
+  "index.html",
+  "banks.html",
+  "gold.html",
+  "zakat.html",
+  "debts.html",
+  "settings.html",
+
+  "style.css",
+  "core_logic.js",
+  "banks.js",
+  "debts.js",
+  "gold.js",
+  "zakat.js",
+  "settings.js",
+
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+// تثبيت الكاش لأول مرة
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// جلب الملفات من الكاش أولاً ثم الشبكة
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return (
+        res ||
+        fetch(event.request).then(fetchRes => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
+});
+
+// تحديث الكاش عندما تصدر نسخة جديدة
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
